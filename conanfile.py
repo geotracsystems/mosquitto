1e818612da540e1fa3a220f3548252e2877073be
Daniel Mather <daniel_mather@trimble.com>
2021-01-29 14:51:37 -0700

    Update conan version to 1.6.10

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class LibmosquittoConan(ConanFile):
     name = "libmosquitto"
-    version = "1.5.4-togs2"
+    version = "1.6.10-togs1"
     license = "EDL/EPL"
     description = "Mosquitto MQTT client library"
     url = "https://github.com/geotracsystems/mosquitto"
